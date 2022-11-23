71d080008a9e6663541926527514704766030724
carlesnacher <carlesnacher@gmail.com>
2022-11-23 10:28:27 +0100

    Añadiendo estilos a los componentes

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MenuIzqComponent } from './menu-izq/menu-izq.component';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    MenuIzqComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
 
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    MenuIzqComponent
   ]
 })
 export class ElementsModule { }
